e977ce39ff702b1fa9d6434543e978c1e4cc1adf
Shaphil <shaphil2025@gmail.com>
2017-11-14 12:17:19 +0600

    Add POSTing of a Task

--- tasklogger_api/resources/Task.py
+++ tasklogger_api/resources/Task.py
@@ -1,5 +1,6 @@
-from flask_restful import Resource, fields, marshal_with
+from flask_restful import abort, Resource, fields, marshal_with, reqparse
 
+from tasklogger_api import db
 from tasklogger_api.models.task import Task
 
 
@@ -11,7 +12,13 @@ task_fields = {
 }
 
 
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('date')
+
+
 class TaskApi(Resource):
+    """ Deal with single task """
     @marshal_with(task_fields)
     def get(self, id):
         task = Task.query.filter_by(id=id).first()
@@ -19,7 +26,20 @@ class TaskApi(Resource):
 
 
 class TasksListApi(Resource):
+    """ Deal with a list of tasks """
     @marshal_with(task_fields)
     def get(self):
         tasks = Task.query.all()
         return tasks
+
+    @marshal_with(task_fields)
+    def post(self):
+        args = parser.parse_args()
+        task_name = args['name']
+        if task_name:
+            task = Task(name=task_name)
+            db.session.add(task)
+            db.session.commit()
+            return task, 201
+
+        return abort(400)
